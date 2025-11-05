397365d2e7c97b0eb3f28f53d93dadbd7d3f7e07
Edixon Sánchez <ediiixonsanchezg@gmail.com>
2025-11-05 14:11:33 +0100

    ejercicio 28 completado

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,11 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        try {
+            throw new Exception("Excepción controlada en main");
+        } catch (Exception e) {
+            System.out.println("Se ha capturado una excepción: " + e.getMessage());
+        }
 
     }
 }
